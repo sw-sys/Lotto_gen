80b02608f55ebf69b95f16de85dab09a59797b4a
sw-sys <sheldonkorpet@gmail.com>
2023-05-03 20:33:11 +0100

    Interface complete

--- number_generator.py
+++ number_generator.py
@@ -1,10 +1,10 @@
 from tkinter import *
 
-# create objects
+# create objects - window and logo
 window = Tk()
 img = PhotoImage(file = 'lotto.gif')
 
-# create widgets
+# create widgets for numbers and buttons
 imgLbl = Label(window, image = img)
 label1 = Label(window,relief = 'groove', width = 2)
 label2 = Label(window, relief = 'groove', width = 2)
@@ -27,6 +27,22 @@ label6.grid(row = 1, column = 7, padx = (10, 20))
 
 getBtn.grid(row = 2, column = 2, columnspan = 4)
 resBtn.grid(row = 2, column = 6, columnspan = 2)
+
+# static properties - never change
+window.title('Lotto Number Picker')
+    # prevents window resizing (width, height)
+window.resizable(0,0)
+getBtn.configure(text = 'Get My Lucky Numbers')
+resBtn.configure(text = 'Reset')
+
+#initial properties for numbers
+label1.configure(text='...')
+label2.configure(text='...')
+label3.configure(text='...')
+label4.configure(text='...')
+label5.configure(text='...')
+label6.configure(text='...')
+resBtn.configure(state = DISABLED)
 
 # loop sustains the window
 window.mainloop()
